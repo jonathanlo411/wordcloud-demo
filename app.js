ad32b1c4a3b04614c124a9b78f970b53330299a1
Jonathan Lo <jonathanlo411@gmail.com>
2023-12-13 12:38:32 -0800

    feat: add user controls

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 
 // Self Launch
+document.querySelector('#transcipt-option').addEventListener("change", loadWordCloud)
+document.querySelector('#stopwords').addEventListener("keyup", loadWordCloud)
 loadWordCloud('202104_ComposableSystems-webinar.json', [])
 
-async function loadWordCloud(transcriptID, additionalStopwords) {
+async function loadWordCloud() {
     const target = document.querySelector('#word-cloud-target');
     const error = document.querySelector('#error');
+    const transcriptSelect = document.querySelector('#transcipt-option')
+    const additionalStopwords = document.querySelector('#stopwords')
+
+    console.log(additionalStopwords.value.split(','))
 
     if (!WordCloud.isSupported) {
         target.style.display = 'none';
         error.style.display = 'block';
     } else {
         // Load data
-        const rawTranscript = await fetch('./transcript.json');
+        const rawTranscript = await fetch(`./transcripts/${transcriptSelect.value}`);
         const rawStopwords = await fetch('./ignore.txt');
-        console.log(rawTranscript)
         const transcript = await rawTranscript.json();
-        const stopwords = (await rawStopwords.text()).split(',');
+        let stopwords = (await rawStopwords.text()).split(',');
 
-        console.log(transcript)
         // Data Processing
+        stopwords = stopwords.concat(additionalStopwords.value.split(','))
         let corpus = transcript.map((d) => d.text.split(' ')).flat(1);
         corpus = corpus.filter(value => !stopwords.includes(value))
         const sortedCorpus = Object.entries(countUniqueWords(corpus)).sort((a, b) => b[1] - a[1]);
